bff3a21a3a533d5e38c137c680570ae8147b88c3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 12:10:56 -0700

    Fixed all the functions

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return num1 + num2
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+    return num1 - num2
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+    return num1 * num2
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    return num1/float(num2)
+
+def square(num):
+    """Return the square of a number"""
+    return num * num
+
+def cube(num):
+    """Return the cube of a number"""
+    return num * num * num
+
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    return num**exponent
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+    return num1%num2
